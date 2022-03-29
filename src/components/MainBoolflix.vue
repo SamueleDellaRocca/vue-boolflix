49d4e1b4ec75da09e96c413cb15ad672ceda738e
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-29 17:18:05 +0200

    splash page

--- src/components/MainBoolflix.vue
+++ src/components/MainBoolflix.vue
@@ -1,12 +1,15 @@
 <template>
   <main>
-    <h1 class="text-center pt-5 rosso">MOVIES</h1>
-    <div class="row gap-5 text-center justify-content-center pb-5">
-      <card-film
-        v-for="cartaFilm in ArrayData"
-        :key="cartaFilm.id"
-        :carta-film-data="cartaFilm"
-      />
+    <h1 v-if="ArrayData.length == 0" class="intro">Cerca Qualcosa</h1>
+    <div v-else>
+      <h1 class="text-center pt-5 rosso">MOVIES</h1>
+      <div class="row gap-5 text-center justify-content-center pb-5">
+        <card-film
+          v-for="cartaFilm in ArrayData"
+          :key="cartaFilm.id"
+          :carta-film-data="cartaFilm"
+        />
+      </div>
     </div>
   </main>
 </template>
@@ -32,6 +35,7 @@ export default {
 main {
   min-height: calc(100vh - 100px);
   background-color: darkgray;
+  position: relative;
 }
 
 .row {
@@ -41,5 +45,13 @@ main {
 .rosso {
   color: black;
   font-size: 4rem;
+}
+
+.intro {
+  font-size: 4rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>

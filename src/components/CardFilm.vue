17517261dcbd1f0cdffc1b5afcb9e7b43457c058
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-29 19:16:56 +0200

    bandiere

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -2,22 +2,30 @@
   <div class="col-2 card mt-5 carta-film">
     <h1>{{ CartaFilmData.title }}</h1>
     <h2>{{ CartaFilmData.original_title }}</h2>
-    <h3>{{ CartaFilmData.original_language }}</h3>
+    <h3>
+      <lang-flag :iso="CartaFilmData.original_language" :squared="false" />
+    </h3>
     <h4>{{ CartaFilmData.vote_average }}</h4>
   </div>
 </template>
 
 <script>
+import LangFlag from "vue-lang-code-flags";
+
 export default {
   name: "CartaFilm",
 
   props: {
     CartaFilmData: Object,
   },
+
+  components: {
+    LangFlag,
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .carta-film {
   background-color: black;
   color: white;
--- src/components/CartaSerie.vue
+++ src/components/CartaSerie.vue
@@ -2,17 +2,25 @@
   <div class="col-2 card mt-5 carta-serie">
     <h1>{{ CartaSerieData.name }}</h1>
     <h2>{{ CartaSerieData.original_name }}</h2>
-    <h3>{{ CartaSerieData.original_language }}</h3>
+    <h3>
+      <lang-flag :iso="CartaSerieData.original_language" :squared="false" />
+    </h3>
     <h4>{{ CartaSerieData.vote_average }}</h4>
   </div>
 </template>
 
 <script>
+import LangFlag from "vue-lang-code-flags";
+
 export default {
   name: "CartaSerie",
 
   props: {
     CartaSerieData: Object,
+  },
+
+  components: {
+    LangFlag,
   },
 };
 </script>

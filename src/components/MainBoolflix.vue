83744ff9c4a3371b8a21c1e0d23013146303335d
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-29 11:58:56 +0200

    layout base

---
+++ src/components/MainBoolflix.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>main</main>
+</template>
+
+<script>
+export default {
+  name: "MainBoolflix",
+
+  data() {
+    return;
+  },
+};
+</script>
+
+<style>
+main {
+  min-height: calc(100vh - 100px);
+  background-color: darkgray;
+}
+</style>
